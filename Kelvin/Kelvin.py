c2abc1552f0f0b6089a00ae1b2a7716c81a2a6bc
Wouter Lindeboom <Wouterlindeboom@hotmail.lv>
2015-09-23 11:55:45 +0200

    Kelvin update
    Updated Kelvin script to restart after answer is given

--- Kelvin/Kelvin.py
+++ Kelvin/Kelvin.py
@@ -1,10 +1,13 @@
-﻿f = input("How many degrees celsius? ")
+﻿while True:
+    f = input("How many degrees celsius? ")
 
-k = f + 273.15
-if k == 0:
-    print "Absolute zero"
-else:
-    print k
+    k = f + 273.15
+    if k == 0:
+        print "Absolute zero"
+    elif k < 0:
+        print "Value can not be below absolute zero"
+    else:
+        print k
 
 
 
