8c5e1bc9f4c5723b998f17e7ef9e30e7cd7ffdfe
Wouter Lindeboom <Wouterlindeboom@hotmail.lv>
2015-10-06 13:23:59 +0200

    Made CaesarCipher work if the result string would be above z

--- Assignment V/CaesarCipher/CaesarCipher/CaesarCipher.py
+++ Assignment V/CaesarCipher/CaesarCipher/CaesarCipher.py
@@ -1,10 +1,19 @@
-string = raw_input("Enter a word to decode: ")
+﻿string = raw_input("Enter a word to decode: ")
 key = int(raw_input("Enter a key: "))
 encodedString = ""
+string = string.lower()
+
 
 for character in string:
     if character.isalpha():
-        character = chr(ord(character) + key)
-    encodedString += character
+        result = ord(character) + key
+        if result > 122:
+            abovez = result - 122
+            character = 96 + abovez
+            char = chr(character)
+        else:
+            char = chr(ord(character) + key)
+    encodedString += char
+
 
 print(encodedString)
